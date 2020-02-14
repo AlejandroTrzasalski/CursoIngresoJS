4e4f6a4814e32b7f74c8e504ef7cdc659e73dbc1
AlejandroTrzasalski <60760259+AlejandroTrzasalski@users.noreply.github.com>
2020-02-13 22:21:44 -0300

    13/2 If y Random

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,6 +2,19 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad
+
+edad = parseInt(document.getElementById("edad").value);
+
+if(edad <13 ) {
+    alert("Sos Niño");
+}
+else if (edad <=17 ){
+    alert("Sos Adolescente");
+}
+else{
+    alert("Sos Adulto");
+}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,22 @@
-function mostrar()
-{
-//tomo la edad  
+function mostrar() {
+    //tomo la edad  
 
-	
+    var edad
+    var EstadoCivil 
+    
+    edad = parseInt(document.getElementById("edad").value);
+    EstadoCivil = document.getElementById("estadoCivil").value;
 
+    if (edad < 18 && EstadoCivil != "Soltero")
+     {
+        alert("Es muy pequeño para ser soltero")
+    }
 
-}//FIN DE LA FUNCIÓN
+   
+        
+
+    
+ 
+
+}
+//FIN DE LA FUNCIÓN
