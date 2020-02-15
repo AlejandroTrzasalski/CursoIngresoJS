9c02ed390bb094ef48047cd65ec810fd83831763
AlejandroTrzasalski <60760259+AlejandroTrzasalski@users.noreply.github.com>
2020-02-14 22:19:12 -0300

    switch - while

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
+switch(mes){
+    case "Enero":
+        alert("Que comiences bien el año");
+        break;
+    
+    case "Marzo":
+        alert("A clases!!");
+        break;
 
+    case "Julio":
+        alert("Se vienen las vacaiones!!");
+        break;
+
+    case "Diciembre":
+        alert("Felices Fiestas!");
+        break;
+
+        
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,33 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById("mes").value;
 
 //alert (mesDelAño);
 	
 	
+switch(mes){
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+        alert("Este mes tiene 31 dias");
+        break;
 
+    case "Febrero":
+        alert("Este mes tiene 29 dias");
+        break;
+
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+        alert("Este mes tiene 30 dias");
+        break;
+}
 
 
 }//FIN DE LA FUNCIÓN
